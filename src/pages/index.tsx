c59a8f1f76cdcd12e489f0e041a64c3a91e6ca90
kammers <marcokammer59@gmail.com>
2022-08-25 13:28:08 +0300

    prisma get projects fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home({
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   const { data: projectData, isLoading } = trpc.proxy.project.all.useQuery();
 
-  // if (isLoading) return <div>Loading...</div>;
+  if (isLoading) return <div>Loading...</div>;
 
   return (
     <Suspense fallback={null}>
@@ -50,8 +50,9 @@ export default function Home({
             Projects
           </h3>
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
-            {projectData ? (
-              projectData.map((project, i) => (
+            {
+              // projectData ? (
+              projectData?.map((project, i) => (
                 <a key={i} href={project.link}>
                   <Image
                     className='w-full h-36 lg:h-60 object-cover rounded-xl filter grayscale hover:grayscale-0'
@@ -63,9 +64,11 @@ export default function Home({
                   />
                 </a>
               ))
-            ) : (
-              <p>bruh you ain&#39;t got any data</p>
-            )}
+              // )
+              //  : (
+              //   <p>bruh you ain&#39;t got any data</p>
+              // )
+            }
           </div>
           <h3 className='font-bold text-2xl md:text-4xl tracking-tight mb-6 mt-16'>
             Featured Posts
