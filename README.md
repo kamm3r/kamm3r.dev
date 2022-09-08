23de749004089a71f5378c386d9bfc93e2914477
kammers <marcokammer59@gmail.com>
2022-09-08 20:56:40 +0300

    minor fixes

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Person Portfolio
 
-Next.js with tRPC prisma etc.
+T3 Stack
 
 ## Setup
 
@@ -18,19 +18,10 @@ pnpm dev
 
 # Tasks TODO
 
-- [x] add all project to the site
-- [x] Contact Resposiveness n alignment fixes
-- [x] responsiveness on all size check up n then fixing if needed
 - [] performance tweaks n minor fixes
-- [x] add video and gif to tweets
 - [] fix video and gif to tweets in mobile and so that it works on firefox
-- [] fixes view adding up to persist
 - [] make light/dark mode favicon and logo
-- [x] fix prod newsletter submitting
-- [x] contact section finishing done
-- [x] project layout with 2 columns and mappping it or for loop it done
-- [x] fix homepage project not loading
-- [] Add cookie user feature?
+- [] add ui element states
 
 ## look into
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
-import type { GetStaticProps, InferGetStaticPropsType, NextPage } from 'next';
+import type { InferGetStaticPropsType } from 'next';
 import Image from 'next/future/image';
 import Link from 'next/link';
+import { Suspense } from 'react';
 import Layout from '../components/Layout';
 import BlogPostCard from '../components/BlogPostCard';
-import { Newsletter } from '../components/Newsletter';
 import { trpc } from '../utils/trpc';
 import { getAllPosts, Post } from '../utils/mdx';
 
@@ -13,101 +13,101 @@ export default function Home({
   const { data: projectData, isLoading } = trpc.project.all.useQuery();
 
   return (
-    // <Suspense fallback={null}>
-    <Layout>
-      <div className='flex flex-col justify-center items-start max-w-5xl border-gray-200 dark:border-gray-700 mx-auto pb-16'>
-        <div className='flex flex-col-reverse justify-between sm:flex-row items-start w-full gap-8'>
-          <div className='flex flex-col w-full '>
-            <h1 className='font-bold text-3xl md:text-5xl tracking-tight mb-1 text-black dark:text-white'>
-              Marco Kammer
-            </h1>
-            <h2 className='text-gray-700 dark:text-gray-200 mb-4'>
-              VP of Developer Experience at{' '}
-              <span className='font-semibold'>Athlete</span>
-            </h2>
-            <p className='text-gray-600 dark:text-gray-400 mb-16'>
-              Making developer concepts simpler and easier to digest. Spreading
-              the T3 stack gospel aka OP stack
-            </p>
-          </div>
-          <div className='w-[80px] sm:w-[176px] relative mb-8 sm:mb-0 '>
-            <Image
-              alt='Marco Kammer'
-              height={176}
-              width={176}
-              src='/profile.jpg'
-              sizes='30vw'
-              priority
-              className='rounded-full filter grayscale'
-            />
-          </div>
-        </div>
-
-        <h3 className='font-bold text-2xl md:text-4xl tracking-tight mb-4 '>
-          Projects
-        </h3>
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-full'>
-          {!projectData || isLoading
-            ? Array.from({ length: 6 }, (_, i) => (
-                <div
-                  key={i}
-                  className='space-y-5 rounded-2xl bg-white/5 p-4 w-full max-w-[1280px] max-h-[720px] flex-[1_1_30%]'
-                >
-                  <div className='h-24 rounded-lg bg-rose-100/10'></div>
-                  <div className='space-y-3'>
-                    <div className='h-3 w-3/5 rounded-lg bg-rose-100/10'></div>
-                    <div className='h-3 w-4/5 rounded-lg bg-rose-100/20'></div>
-                    <div className='h-3 w-2/5 rounded-lg bg-rose-100/20'></div>
-                  </div>
-                </div>
-              ))
-            : projectData.map((project, i) => (
-                <a key={i} href={project.link}>
-                  <Image
-                    className='w-full h-36 lg:h-60 object-cover rounded-xl filter grayscale hover:grayscale-0'
-                    src={project.image}
-                    alt={project.name}
-                    priority
-                    width={1280}
-                    height={720}
-                  />
-                </a>
-              ))}
-        </div>
-        <h3 className='font-bold text-2xl md:text-4xl tracking-tight mb-6 mt-16'>
-          Featured Posts
-        </h3>
-        <div className='flex gap-6 flex-col md:flex-row'>
-          {posts?.map((post) => (
-            <BlogPostCard
-              key={post._id}
-              title={post.title}
-              slug={post.slug}
-              // gradient='from-[#FDE68A] via-[#FCA5A5] to-[#FECACA]'
-              gradient='from-[#D8B4FE] to-[#818CF8]'
-            />
-          ))}
-        </div>
-        <Link href='/blog'>
-          <a className='flex items-center mt-8 text-gray-600 dark:text-gray-400 leading-7 rounded-lg hover:text-gray-800 dark:hover:text-gray-200 transition-all h-6'>
-            Read all posts
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              viewBox='0 0 24 24'
-              className='h-6 w-6 ml-1'
-            >
-              <path
-                stroke='currentColor'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth={2}
-                d='M17.5 12h-15m11.667-4l3.333 4-3.333-4zm3.333 4l-3.333 4 3.333-4z'
+    <Suspense fallback={null}>
+      <Layout>
+        <div className='flex flex-col justify-center items-start max-w-5xl border-gray-200 dark:border-gray-700 mx-auto pb-16'>
+          <div className='flex flex-col-reverse justify-between sm:flex-row items-start w-full gap-8'>
+            <div className='flex flex-col w-full '>
+              <h1 className='font-bold text-3xl md:text-5xl tracking-tight mb-1 text-black dark:text-white'>
+                Marco Kammer
+              </h1>
+              <h2 className='text-gray-700 dark:text-gray-200 mb-4'>
+                VP of Developer Experience at{' '}
+                <span className='font-semibold'>Athlete</span>
+              </h2>
+              <p className='text-gray-600 dark:text-gray-400 mb-16'>
+                Making developer concepts simpler and easier to digest.
+                Spreading the T3 stack gospel aka OP stack
+              </p>
+            </div>
+            <div className='w-[80px] sm:w-[176px] relative mb-8 sm:mb-0 '>
+              <Image
+                alt='Marco Kammer'
+                height={176}
+                width={176}
+                src='/profile.jpg'
+                sizes='30vw'
+                priority
+                className='rounded-full filter grayscale'
               />
-            </svg>
-          </a>
-        </Link>
-        {/* <h3 className='font-bold text-2xl md:text-4xl tracking-tight mb-4 mt-16 text-black dark:text-white'>
+            </div>
+          </div>
+
+          <h3 className='font-bold text-2xl md:text-4xl tracking-tight mb-4 '>
+            Projects
+          </h3>
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-full'>
+            {!projectData || isLoading
+              ? Array.from({ length: 6 }, (_, i) => (
+                  <div
+                    key={i}
+                    className='space-y-5 rounded-2xl bg-white/5 p-4 w-full max-w-[1280px] max-h-[720px] flex-[1_1_30%]'
+                  >
+                    <div className='h-24 rounded-lg bg-rose-100/10'></div>
+                    <div className='space-y-3'>
+                      <div className='h-3 w-3/5 rounded-lg bg-rose-100/10'></div>
+                      <div className='h-3 w-4/5 rounded-lg bg-rose-100/20'></div>
+                      <div className='h-3 w-2/5 rounded-lg bg-rose-100/20'></div>
+                    </div>
+                  </div>
+                ))
+              : projectData.map((project, i) => (
+                  <a key={i} href={project.link}>
+                    <Image
+                      className='w-full h-36 lg:h-60 object-cover rounded-xl filter grayscale hover:grayscale-0'
+                      src={project.image}
+                      alt={project.name}
+                      priority
+                      width={1280}
+                      height={720}
+                    />
+                  </a>
+                ))}
+          </div>
+          <h3 className='font-bold text-2xl md:text-4xl tracking-tight mb-6 mt-16'>
+            Featured Posts
+          </h3>
+          <div className='flex gap-6 flex-col md:flex-row'>
+            {posts?.map((post) => (
+              <BlogPostCard
+                key={post._id}
+                title={post.title}
+                slug={post.slug}
+                // gradient='from-[#FDE68A] via-[#FCA5A5] to-[#FECACA]'
+                gradient='from-[#D8B4FE] to-[#818CF8]'
+              />
+            ))}
+          </div>
+          <Link href='/blog'>
+            <a className='flex items-center mt-8 text-gray-600 dark:text-gray-400 leading-7 rounded-lg hover:text-gray-800 dark:hover:text-gray-200 transition-all h-6'>
+              Read all posts
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                className='h-6 w-6 ml-1'
+              >
+                <path
+                  stroke='currentColor'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth={2}
+                  d='M17.5 12h-15m11.667-4l3.333 4-3.333-4zm3.333 4l-3.333 4 3.333-4z'
+                />
+              </svg>
+            </a>
+          </Link>
+          {/* <h3 className='font-bold text-2xl md:text-4xl tracking-tight mb-4 mt-16 text-black dark:text-white'>
           Learn React & Next.js
         </h3>
         <p className='text-gray-600 dark:text-gray-400 mb-4'>
@@ -137,11 +137,11 @@ export default function Home({
             />
           </svg>
         </a> */}
-        {/* <span className='h-16' /> */}
-        {/* <Newsletter /> */}
-      </div>
-    </Layout>
-    // </Suspense>
+          {/* <span className='h-16' /> */}
+          {/* <Newsletter /> */}
+        </div>
+      </Layout>
+    </Suspense>
   );
 }
 
