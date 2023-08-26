528ae096ff80688f18a0af1694013b2bf6b5c0ac
Marco <marcokammer59@gmail.com>
2023-08-26 16:06:02 +0300

    finish basic structure

--- src/utils/getPinnedRepo.ts
+++ src/utils/getPinnedRepo.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 const projectSchema = z.object({
-  repo: z.string(),
+  name: z.string(),
   link: z.string(),
   description: z.string(),
 });
@@ -11,7 +11,7 @@ export type Project = z.infer<typeof projectSchema>;
 export async function getPinnedRepos(
   number: 1 | 2 | 3 | 4 | 5 | 6
 ): Promise<Project[]> {
-  const res = await fetch("https://gh-pinned.vercel.app/api/user/thrzl").then(
+  const res = await fetch("https://pinned.thrzl.xyz/kamm3r").then(
     (res) => res.json()
   );
 
