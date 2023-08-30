231943d848883e7083bb6b7835b7487a92908a76
Marco Kammer <kammmar@DMAC026.local>
2023-08-30 12:53:46 +0300

    added isDraft feature to blog post

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,10 +3,8 @@ import { z } from "zod"
 
 const blog = defineCollection({
     type: "content",
-
     schema: z.object({
         isDraft: z.boolean().optional().default(false),
-
         title: z.string(),
         description: z.string(),
         date: z.string().transform(str => new Date(str)),
