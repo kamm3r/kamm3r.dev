cb6dc33125384fa40e21cbf0d86111395042513d
Marco Kammer <kammmar@DMAC026.local>
2023-08-30 13:09:59 +0300

    vercel analytics

--- astro.config.ts
+++ astro.config.ts
@@ -1,15 +1,21 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://kamm3r.dev',
   integrations: [mdx({
     syntaxHighlight: 'shiki',
-    shikiConfig: { theme: 'poimandres' }
-  }),
-  sitemap(), tailwind()]
+    shikiConfig: {
+      theme: 'poimandres'
+    }
+  }), sitemap(), tailwind()],
+  output: "server",
+  adapter: vercel({
+    analytics: true,
+  })
 });
